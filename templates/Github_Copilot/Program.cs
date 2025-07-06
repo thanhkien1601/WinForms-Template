9cc50d3efb30acc3998782a213bf976393f76787
Anh Nguyen <74936256+nguyenanhvn9@users.noreply.github.com>
2025-07-06 08:39:45 +0700

    Bài thực hành để sử dụng Github Copilot

---
+++ templates/Github_Copilot/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        // Create two lists of arrays not duplicating elements
+        List<int> list1 = [3, 1, 4, 5, 9, 2, 6];
+        List<int> list2 = [2, 7, 1, 8, 0, 21];
+
+        // Sort both lists
+        list1.Sort();
+        list2.Sort();
+
+        Console.WriteLine($"Sorted List 1: [{string.Join(", ", list1)}]");
+        Console.WriteLine($"Sorted List 2: [{string.Join(", ", list2)}]");
+
+        // Find intersection
+        var intersection = FindIntersection(list1.ToArray(), list2.ToArray());
+
+        Console.WriteLine($"Intersection: [{string.Join(", ", intersection)}]");
+        Console.ReadLine();
+    }
+
+    static int[] FindIntersection(int[] array1, int[] array2)
+    {
+        var intersection = new List<int>();
+
+        for (int i = 0; i < array1.Length; i++)
+        {
+            for (int j = 0; j < array2.Length; j++)
+            {
+                if (array1[i] == array2[j])
+                {
+                    intersection.Add(array1[i]);
+                }
+            }
+        }
+
+        return intersection.ToArray();
+    }
+}
